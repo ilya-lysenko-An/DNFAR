37afd72539c295399c7e8550b883593702d54a67
Ilya Lysenko <ilyalysenko7828@gmail.com>
2026-01-31 16:55:04 +0300

    Подготовка массива входных данных

--- dsp.cpp
+++ dsp.cpp
@@ -10,3 +10,17 @@ static double gaussianNoise(double sigma) {
     return sigma * dist(gen);
 }
 
+std::vector<std::vector<cdouble>> generateAntennaSignals(
+    const std::vector<Source>& sources,
+    int N,
+    double R,
+    double fc,
+    double Fs,
+    int L,
+    double SNR
+) {
+        std::vector<std::vector<cdouble>> signals(
+        N, std::vector<cdouble>(L, {0.0, 0.0})
+    );
+}
+
