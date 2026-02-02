78a1f5820ba973c6f93d44d6e6217152df9a7240
Ilya Lysenko <ilyalysenko7828@gmail.com>
2026-02-02 13:05:35 +0300

    Добавил сценарий в CMake, сделал проверку работы одной антенны

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "dsp.h"
+#include <iostream>
+#include <cmath>
 
 int main() {
     std::vector<Source> sources = {
@@ -6,6 +8,31 @@ int main() {
         {17e6, 140.0, 1.0}
     };
 
-    auto signals = generateAntennaSignals(sources, 8, 28.0, 15e6, 50e6, 4096, 10.0);
+    int N = 8;
+    int L = 4096 ;
+    double Fs = 50e6;
 
+    auto signals = generateAntennaSignals(
+        sources,
+        N,
+        28,0,
+        15e6,
+        Fs,
+        L,
+        10.0
+    );
+    auto& x = signals[0];
+
+    auto X = fft(x);
+
+    for (int k = 0; k < L / 2; ++k){
+        double freq = k * Fs / L;
+        double mag = std::abs(X[k]);
+        if (mag > 50.0) { // порог для наглядности
+            std::cout << "f = " << freq / 1e6
+                      << " MHz, |X| = " << mag << std::endl;
+    }
+
+}
+return 0;
 }
